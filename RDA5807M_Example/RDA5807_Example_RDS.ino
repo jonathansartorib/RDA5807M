70bc4622e131da65267661f3145a394363026c16
Jonathan Sartori Bruzarrosco <jonathansartorib@hotmail.com>
2023-11-06 10:07:26 -0300

    Update RDA5807_Example_RDS example

--- RDA5807M_Example/RDA5807_Example_RDS.ino
+++ RDA5807M_Example/RDA5807_Example_RDS.ino
@@ -2,7 +2,7 @@
  * RDA5807M Example Sketch
  *
  * This example sketch demonstrates how to use the RDA5807M Library to interact with the RDA5807M FM radio module.
- * The sketch initializes the RDA5807M in West-FM mode (87-108MHz), waits for RDS data to become available,
+ * The sketch initializes the RDA5807M in West-FM mode (87-108MHz), reads the current frequency, waits for RDS data to become available,
  * and then displays the received station name and RDS block information via the serial port.
  *
  * HARDWARE SETUP:
@@ -20,39 +20,52 @@
 // Add the RDA5807M Library to the sketch.
 #include <RDA5807M.h>
 
+#include <RDA5807M.h>
+
 RDA5807M radio;
 
 void setup()
 {
-    Serial.begin(9600);
-    radio.init();
-    radio.setFrequency(100.0); // Set the desired frequency in MHz
-    radio.setVolume(10);       // Set the desired volume (0-15)
+    // Initialize communication with the RDA5807M module
+    radio.begin(RDA5807M_BAND_WEST); // Choose the west band
 
-    // Enable RDS
-    radio.setRegister(0x02, radio.getRegister(0x02) | (1 << 15)); // Set the RDS_EN bit
+    // Enable RDS data reception
+    radio.updateRegister(RDA5807M_REG_RDS, RDA5807M_FLG_RDS);
+
+    // Initialize the serial monitor
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    radio.update();
+    // Read the current frequency
+    word frequency = radio.getFrequency(); // Frequency in 10 kHz units
 
-    if (radio.getRegister(0x0A) & (1 << 15))
-    {
-        // The RDSR bit in register 0x0A has been set, indicating available RDS data
-        Serial.println("RDS Signal Received!");
-        Serial.print("Station: ");
-        Serial.println(radio.getStationName());
+    // Tune the radio to the current frequency
+    radio.setFrequency(frequency);
 
-        // Read and print RDS blocks (0x0C to 0x0F)
-        for (int i = 0x0C; i <= 0x0F; i++)
-        {
-            Serial.print("RDS Block ");
-            Serial.print(i - 0x0C);
-            Serial.print(": 0x");
-            Serial.println(radio.getRegister(i), HEX);
-        }
+    // Read RDS data blocks A and B
+    word rdsA, rdsB;
+    radio.getRegisterBulk(2, &rdsA);
+    radio.updateRegister(RDA5807M_REG_RDS, RDA5807M_FLG_RDS); // Clear the RDS_READY bit
 
-        Serial.println();
-    }
+    // Decode RDS data (as before)
+
+    // Display the current frequency and RDS data
+    Serial.print("Current Frequency: ");
+    Serial.print(frequency);
+    Serial.println(" kHz");
+    Serial.print("Group Type: ");
+    Serial.println(groupType);
+    Serial.print("Version Code: ");
+    Serial.println(versionCode);
+    Serial.print("Traffic Flag: ");
+    Serial.println(trafficFlag);
+    Serial.print("Program Type: ");
+    Serial.println(programType);
+    Serial.print("Text Segment: ");
+    Serial.println(textSegment);
+
+    // Wait for some time before reading again
+    delay(1000);
 }
